aa0592f997b8bb1a14c5a137d55a887975f300ea
suhitha1809 <suhithasornala999@gmail.com>
2026-04-19 19:22:26 +0530

    Delete frontend directory

--- frontend/src/api.js
+++
@@ -1,6 +0,0 @@
-// Centralized API calls
-export async function apiFetch(url, options = {}) {
-  const res = await fetch(url, options);
-  if (!res.ok) throw new Error('API error');
-  return res.json();
-}
